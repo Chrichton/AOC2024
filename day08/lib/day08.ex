578c1e2f86b9e338c2b7adaddfa2ea67eddaefd7
Heiko Goes <heiko.goes@gaebler-goes.de>
2024-12-23 15:40:24 +0100

    day08 sample works

--- day08/lib/day08.ex
+++ day08/lib/day08.ex
@@ -11,41 +11,64 @@ defmodule Day08 do
     row_count = Enum.count(grid)
     col_count = Enum.count(Enum.at(grid, 0))
 
-    antennas =
+    antenna_name_position_list =
       for x <- 0..(row_count - 1),
           y <- 0..(col_count - 1),
-          Enum.at(Enum.at(grid, x), y) != ".",
-          do: {x, y}
+          antenna_name = Enum.at(Enum.at(grid, x), y),
+          antenna_name != ".",
+          do: {antenna_name, {x, y}}
 
-    antenna_pairs =
-      Enum.group_by(antennas, fn {x, y} ->
-        Enum.at(Enum.at(grid, x), y)
-      end)
+    antennas_combinations =
+      Enum.group_by(
+        antenna_name_position_list,
+        fn {antenna_name, _position} -> antenna_name end,
+        fn {_antenna_name, {x, y}} -> {x, y} end
+      )
+
+    antennas_combinations =
+      antennas_combinations
       |> Enum.flat_map(fn {_antenna, positions} ->
         Combination.combine(positions, 2)
       end)
 
-    antenna_pairs
+    {antenna_name_position_list, antennas_combinations}
   end
 
   def solve(input) do
     input
     |> read_input()
+    |> then(fn {antenna_name_position_list, antennas_combinations} ->
+      {antenna_name_position_list, antennas_combinations}
+
+      antenna_positions_map_set =
+        antenna_name_position_list
+        |> Enum.map(fn {_antenna_name, {x, y}} -> {x, y} end)
+        |> MapSet.new()
+
+      antennas_combinations
+      |> Enum.flat_map(fn [p1, p2] -> anti_nodes(p1, p2) end)
+      |> Enum.uniq()
+      |> Enum.reject(fn {x, y} -> x < 0 or y < 0 end)
+      |> MapSet.new()
+      |> MapSet.difference(antenna_positions_map_set)
+      |> Enum.count()
+    end)
   end
 
   def anti_nodes({x1, y1}, {x2, y2}) when x1 < x2 and y1 < y2 do
-    [{2 * x2 - x1}, {2 * y2 - y1}]
+    [{2 * x1 - x2, 2 * y1 - y2}, {2 * x2 - x1, 2 * y2 - y1}]
   end
 
   def anti_nodes({x1, y1} = p1, {x2, y2} = p2) when x1 > x2 and y1 > y2 do
     anti_nodes(p2, p1)
   end
 
-  def anti_nodes({x1, y1}, {x2, y2}) when x1 < x2 and y1 > y2 do
-    [{2 * x2 - x1}, {2 * y1 - y2}]
+  def anti_nodes({x1, y1} = p1, {x2, y2} = p2) when x1 < x2 and y1 > y2 do
+    anti_nodes(p2, p1)
+    # [{2 * x1 - x2, 2 * y1 - y2}, {2 * x2 - x1, 2 * y2 - y1}]
   end
 
-  def anti_nodes({x1, y1}, {x2, y2}) when x1 > x2 and y1 < y2 do
-    [{2 * x1 - x2}, {2 * y2 - y1}]
+  def anti_nodes({x1, y1} = p1, {x2, y2} = p2) when x1 > x2 and y1 < y2 do
+    [{2 * x2 - x1, 2 * y2 - y1}, {2 * x1 - x2, 2 * y1 - y2}]
   end
 end
--- day08/test/day08_test.exs
+++ day08/test/day08_test.exs
@@ -2,6 +2,7 @@ defmodule Day08Test do
   use ExUnit.Case
   doctest Day08
 
+  @tag :skip
   test "read_input" do
     assert Day08.read_input("sample") == [
              [{4, 4}, {3, 7}],
@@ -31,7 +32,7 @@ defmodule Day08Test do
       ..........
       """
 
-    Day08.anti_nodes({3, 4}, {5, 5}) == [{1, 3}, {7, 6}]
+    assert Day08.anti_nodes({3, 4}, {5, 5}) == [{1, 3}, {7, 6}]
   end
 
   test "anti_nodes2" do
@@ -43,23 +44,24 @@ defmodule Day08Test do
       ..#.........
       """
 
-    Day08.anti_nodes({1, 8}, {2, 5}) == [{0, 11}, {3, 3}]
+    assert Day08.anti_nodes({1, 8}, {2, 5}) == [{0, 11}, {3, 2}]
   end
 
   test "sample" do
-    assert Day08.solve("sample") == nil
+    assert Day08.solve("sample") == 14
   end
 
-  # test "star" do
-  #   assert Day07.solve("star") == 4711
-  # end
+  test "star" do
+    # too high
+    assert Day08.solve("star") == 282
+  end
 
   # test "sample2" do
-  #   assert Day07.solve2("sample") == 6
+  #   assert Day08.solve2("sample") == 6
   # end
 
   # test "star2" do
   #   # too high
-  #   assert Day07.solve2("star") == 1563
+  #   assert Day08.solve2("star") == 1563
   # end
 end
